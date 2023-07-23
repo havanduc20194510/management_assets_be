a007eaa2599e6bc5d730b722cd2ce648126ab5f6
VanDucKun <havanducdeptrai@gmail.com>
2023-07-23 18:02:55 +0700

    change status

---
+++ src/main/java/com/example/manageasset/domain/asset/repositories/AssetRepository.java
@@ -0,0 +1,15 @@
+package com.example.manageasset.domain.asset.repositories;
+
+import com.example.manageasset.domain.asset.models.Asset;
+import com.example.manageasset.domain.shared.models.QueryFilter;
+
+import java.util.List;
+
+public interface AssetRepository {
+    List<Asset> getAll(QueryFilter queryFilter, String searchText, Long categoryId, Integer state);
+    Long countTotal(String searchText, Long categoryId, Integer state);
+    Asset getById(Long id);
+    void save(Asset asset);
+    List<Asset> findByIdIn(List<Long> assetIds);
+    Boolean existedAssetInCategory(Long categoryId);
+}
